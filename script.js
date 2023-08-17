3e10f158982c5c0fefed9ad8d82a74050e3c8364
aami_mm <mukarramos@outlook.com>
2023-08-17 15:28:27 +0530

    Completed Code

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function fibonacci(num) {
-// your code here
+
+function fibonacci(number) {
+    if (number === 1) return 0;
+    else if (number === 2) return 1;
+    var number1 = 0;
+    var number2 = 1;
+    var sum;
+    for (var i = 2; i < number; i++) {
+      sum = number1 + number2;
+      number1 = number2;
+      number2 = sum;
+    }
+    return number2;
 }
 
 module.exports = fibonacci;
