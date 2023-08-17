399530fe890b5b4cfd2b7e8c36091a975742a93d
aami_mm <mukarramos@outlook.com>
2023-08-17 15:43:14 +0530

    Completed Code

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 
 function fibonacci(number) {
-    // if (number === 1){
-    //     return 0;
-    // } 
-    // else if(number === 2) {
-    //         return 1;
-    //     }
-     
-    // var number1 = 0;
-    // var number2 = 1;
-    // var sum;
-    // for (var i = 2; i < number; i++) {
-    //   sum = number1 + number2;
-    //   number1 = number2;
-    //   number2 = sum;
-    // }
-    // return number2;
-    
     if(num==1)
 		return 0;
 	else if(num==2||num==3)
